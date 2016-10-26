f33aca46a41fd6d11e7840e1eb49835e01d64da6
magno <emaildomagno@gmail.com>
2016-10-26 10:57:43 -0200

    Initial Version

---
+++ hwjc01/src/main/java/com/emaildomagno/hwjc01/App.java
@@ -0,0 +1,13 @@
+package com.emaildomagno.hwjc01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World cruel!" );
+    }
+}
